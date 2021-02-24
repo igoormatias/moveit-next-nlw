d4730e33dd498a4734b1a7f08312e77e3945db02
Igor Matias <igorhmatias@gmail.com>
2021-02-24 00:58:23 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/components/Profile.module.css'
+
+export default function Profile() {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/igoormatias.png" alt="Igor Matias"/>
+            <div>
+                <strong>Igor Matias</strong>
+                <p> 
+                    <img src="icons/level.svg" alt="Level"/> 
+                    Level 1</p>
+            </div>
+        </div>
+    )
+}
